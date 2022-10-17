137d113d3f8fe8df6f8c3c740bb6218c202d2b9f
Caelan <caelan_seto@hotmail.com>
2022-10-17 13:09:10 -0400

    missing using

--- SenkouCards/CardCreation.xaml.cs
+++ SenkouCards/CardCreation.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,8 +8,6 @@ using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Text.RegularExpressions;
-using System;
 
 namespace SenkouCards
 {
