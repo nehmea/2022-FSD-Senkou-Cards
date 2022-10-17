9d4cedd811b1c96a8dc42f9fbbb3696418c3f99c
Caelan <caelan_seto@hotmail.com>
2022-10-17 13:21:01 -0400

    Revert "Revert "Merge branch 'main' of https://github.com/nehmea/2022-FSD-AppDev1-project into CreateIndividualCards-Caelan""
    This reverts commit 7d601e5ec584762ccd8c5ed127c93b7ad03b4629.

---
+++ SenkouCards/Validation.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel.DataAnnotations;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace SenkouCards
+//{
+//    public partial class users : IValidatableObject
+//    {
+//        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+//        {
+//            if (username == "")
+//            {
+//                yield return new ValidationResult(
+//                    "Username must not be empty", new[] { nameof(username) }
+//                    );
+//            }
+//        }
+//    }
+//}
